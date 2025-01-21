c7b3bfa1954c7d42f4ccd3a154cdedef5b5ac90f
aghaith <abbasghayth5@gmail.com>
2025-01-22 00:52:32 +0400

    Update cors configurations

--- app.js
+++ app.js
@@ -31,7 +31,13 @@ app.use(
   "/public/uploads",
   express.static(path.join(__dirname + "/public/uploads"))
 );
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.options("*", cors());
 app.use(errorHandler);
 
